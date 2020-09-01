3d7d81233201e2e6878682283c803bcc2a6276d9
Pranav Narang <pnarang1130@gmail.com>
2020-09-01 13:30:28 -0600

    Added react-native hooks and made minor changes to app.js

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello World!</Text>
+      <Image source={require('./assets/icon.png')} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
