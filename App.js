340b75ce00964bde7d72ab0fbdc67a0e05767fce
Pranav Narang <pnarang1130@gmail.com>
2020-09-03 19:55:22 -0600

    added missing text import

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView
+} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World!</Text>
-      <Image source={require('./assets/icon.png')} />
+      <View>
+        <Text>Hello World</Text>
+      </View>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
